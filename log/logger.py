90e656ea7f6d6286fcc6251fd97cc20a771abdf6
HramtsovKostya <hramtsovkostya21012001@gmail.com>
2021-07-03 04:47:07 +0800

    Add logging of program

---
+++ log/logger.py
@@ -0,0 +1,73 @@
+# -------------------------------- LOGGER ---------------------------------
+
+import config as cnf
+import logging as log
+
+# -------------------------------------------------------------------------
+
+class DebugLogger(object):
+    __LOG_FORMAT = 'TIME: [ %(asctime)s ] - LEVEL: [ %(levelname)-8s ]'
+    __LOG_FORMAT += ' - LOCATE: [ %(filename)s / %(funcName)s() / '
+    __LOG_FORMAT += 'LINE: %(lineno)d ] - MESSAGE: [ %(message)-25s ]'
+    
+    #? >>> HELP >>>           Уровни логирования           <<<< HELP <<<<
+    #* 1. NOTSET   =  0 - Все сообщения отключены
+    #* 2. DEBUG    = 10 - Подробная информация для диагностики проблем
+    #* 3. INFO     = 20 - Подтверждение об исправной работе прогрраммы
+    #* 4. WARNING  = 30 - Произошла проблема, но код все еще работает (default)
+    #* 5. ERROR    = 40 - Ошибка при выполнении какой-либо функции
+    #* 6. CRITICAL = 50 - Серьезная ошибка, выполнение кода прервалось
+    #? >>> HELP >>>           Уровни логирования           <<<< HELP <<<<
+    
+    __DATE_FORMAT = '%d.%m.%Y %H:%M:%S'
+    
+    #? >>> HELP >>>           Форматирование даты           <<<< HELP <<<<
+    #* %a — будний день сокращенно (Mon, Tue,…)
+    #* %A — будний день полностью (Monday, Tuesday,…)
+    #* %w — будний день, как десятичное число (1, 2, 3,…)
+    #* %d — день месяца в числах (01, 02, 03,…)
+    #* %b — месяцы сокращенно (Jan, Feb,…)
+    #* %B — название месяцев полностью (January, February,…)
+    #* %m — месяцы в числах (01, 02, 03,…)
+    #* %y — года без века (19, 20, 21)
+    #* %Y — года с веком (2019, 2020, 2021)
+    #* %H — 24 часа в сутки (с 00 до 23)
+    #* %I — 12 часов в сутки (с 01 до 12)
+    #* %p — AM и PM (00-12 и 12-00)
+    #* %M — минуты (от 00 до 59)
+    #* %S — секунды (от 00 до 59)
+    #* %f — миллисекунды (6 десятичных чисел)
+    #? >>> HELP >>>           Форматирование даты           <<<< HELP <<<<
+    
+    @staticmethod
+    def get(name: str):
+        fmt = log.Formatter(
+            fmt=DebugLogger.__LOG_FORMAT,
+            datefmt=DebugLogger.__DATE_FORMAT
+        )
+        
+        file_hdl = DebugLogger.__file_handler(fmt)
+        str_hdl = DebugLogger.__str_handler(fmt)
+        
+        logger = log.getLogger(name)
+        logger.setLevel(log.DEBUG)        
+
+        logger.addHandler(file_hdl)
+        logger.addHandler(str_hdl) 
+        return logger
+    
+    @staticmethod
+    def __file_handler(fmt: log.Formatter):
+        hdl = log.FileHandler(cnf.LOGGING_FILE)
+        hdl.setLevel(log.DEBUG)
+        hdl.setFormatter(fmt)
+        return hdl
+
+    @staticmethod
+    def __str_handler(fmt: log.Formatter):
+        hdl = log.StreamHandler()
+        hdl.setLevel(log.WARNING)
+        hdl.setFormatter(fmt)
+        return hdl
+
+# -------------------------------------------------------------------------
