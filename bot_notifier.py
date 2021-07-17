aa7a59a3b543b501359db61757739b672d03fb53
HramtsovKostya <hramtsovkostya21012001@gmail.com>
2021-07-17 22:01:29 +0800

    Uodated bot_notifier.py

--- bot_notifier.py
+++ bot_notifier.py
@@ -1,5 +1,6 @@
 # ------------------------------- SCHEDULE --------------------------------
 
+import config as cfg
 import pandas as pd
 import schedule as sch
 import time
@@ -9,7 +10,7 @@ from telebot.apihelper import ApiTelegramException
 from datetime import datetime, timedelta
 from threading import Thread
 from chat_bot import ChatBot
-
+from sheet_parser import SheetParser
 
 # -------------------------------------------------------------------------
 
@@ -27,62 +28,71 @@ class BotNotifier(object):
 		print('Рассылка уведомлений остановлена!\n')		
 
 	def __schedule(self):
-		# sch.every(10).minutes.do(self.__notify)
-		sch.every().day.at('21:34').do(self.__notify)
+		sch.every(1).minutes.do(self.__notify)
+		# sch.every().day.at('09:00').do(self.__notify)
 
 		while self.__is_working:
 			sch.run_pending()
 			time.sleep(3)
 
 	def __notify(self):  # sourcery no-metrics
-		subs, groups, admins = self.__get_role_users()
+		# ? Создание экземпляра парсера
+		# parser = SheetParser(
+		# 	file_name=cfg.CREDENTIALS_FILE, 
+		# 	scopes=[cfg.GOOGLE_SHEETS_API, cfg.GOOGLE_DRIVE_API]
+		# )
 
-		for user in subs:
+		# ? Загрузка данных из гугл-таблицы
+		# data = parser.get(cfg.SPREAD_SHEET_ID)
+		# SheetParser.save(data, cfg.SPREAD_SHEET_FILE)
+
+		for user in self.__get_subs():
 			data = self.__get_by_name(ChatBot.sheet(), user.user_name)
 
 			if data is not None:
-				week = self.__next_week(data['Дата начала курса'])
-
-				if week is not None:
-					user_id = user.chat_id
-					text = self.__get_user_notification(week, data)
-					self.__bot.send_message(user_id, text, parse_mode='html')
-
-					for group in groups:
-						group_id = group.chat_id
-						try:
-							self.__bot.get_chat_member(group_id, user_id)
-							self.__bot.send_message(group_id, text, parse_mode='html')
-						except ApiTelegramException:
-							continue
-
-					for admin in admins:
-						admin_id = admin.chat_id
-						if admin_id != user_id:
-							text = self.__get_admin_notification(week, data, admin.user_name)
-							self.__bot.send_message(admin_id, text, parse_mode='html')
-				
-				if self.__is_master_class(data['Мастер-класс']):
-					user_id = user.chat_id
-					text = self.__get_user_mc_notification(data)
-					self.__bot.send_message(user_id, text, parse_mode='html')
-	
-					for group in groups:
-						group_id = group.chat_id
-						try:
-							self.__bot.get_chat_member(group_id, user_id)
-							self.__bot.send_message(group_id, text, parse_mode='html')
-						except ApiTelegramException:
-							continue
-
-					for admin in admins:
-						admin_id = admin.chat_id
-						if admin_id != user_id:
-							text = self.__get_admin_mc_notification(data, admin.user_name)
-							self.__bot.send_message(admin_id, text, parse_mode='html')
- 
+				self.__notify_about_lessons(user, data)	
+				self.__notify_about_master_class(user, data) 
 				print('Пользователь ' + user.user_name + ' уведомлен')
+
 		print('Все уведомления разосланы!') 
+
+	def __notify_about_lessons(self, user, data):
+		week = self.__next_week(data['Дата начала курса'])
+
+		if week is not None:
+			text = self.__get_user_notification(week, data)
+			self.__bot.send_message(user.chat_id, text, parse_mode='html')
+
+			for g in self.__get_groups():
+				try:
+					self.__bot.get_chat_member(g.chat_id, user.chat_id)
+					self.__bot.send_message(g.chat_id, text, parse_mode='html')
+				except ApiTelegramException:
+					continue
+
+			for a in self.__get_admins():
+				if a.chat_id != user.chat_id:
+					text = self.__get_admin_notification(week, data, a.user_name)
+					self.__bot.send_message(a.chat_id, text, parse_mode='html')
+
+	def __notify_about_master_class(self, user, data):
+		is_master_class = self.__is_master_class(data['Мастер-класс']) 
+
+		if is_master_class:
+			text = self.__get_user_mc_notification(data)
+			self.__bot.send_message(user.chat_id, text, parse_mode='html')
+			
+			for g in self.__get_groups():
+				try:
+					self.__bot.get_chat_member(g.chat_id, user.chat_id)
+					self.__bot.send_message(g.chat_id, text, parse_mode='html')
+				except ApiTelegramException:
+					continue
+
+			for a in self.__get_admins():
+				if a.chat_id != user.chat_id:
+					text = self.__get_admin_mc_notification(data, a.user_name)
+					self.__bot.send_message(a.chat_id, text, parse_mode='html')
 
 # -------------------------------------------------------------------------
 
@@ -157,16 +167,13 @@ class BotNotifier(object):
 		dt = datetime.strptime(date, '%Y-%m-%d') + timedelta(weeks=number-1)
 		return dt.strftime('%d.%m.%Y') + 'г.'
 
-	def __get_role_users(self):
-		subs, groups, admins = [], [], []
-		for user in ChatBot.users():
-			if user.is_subscriber():
-				subs.append(user)
-			if user.is_admin():
-				admins.append(user)
-			if user.is_group():
-				groups.append(user)
-			
-		return subs, groups, admins
+	def __get_subs(self):
+		return [u for u in ChatBot.users() if u.is_subscriber()]
 
+	def __get_groups(self):
+		return [u for u in ChatBot.users() if u.is_group()]
+
+	def __get_admins(self):
+		return [u for u in ChatBot.users() if u.is_admin()]
+			
 # -------------------------------------------------------------------------
